5f15dc644d2f40f43e1b1341b1b8a5a99e6366bc
sonquer <sonquer@o2.pl>
2020-05-15 17:08:51 +0200

    Fixed main issues

--- tests/Html2Pdf.test.ts
+++ tests/Html2Pdf.test.ts
@@ -1,8 +1,8 @@
 import Html2Pdf from '../lib/Html2Pdf';
 
-describe('Html2Pdf', function() {
-    it('convertToBuffer', async function() {
-        let html2Pdf = new Html2Pdf();
+describe('Html2Pdf', () => {
+    it('convertToBuffer', async () => {
+        const html2Pdf = new Html2Pdf();
         const buffer = await html2Pdf.convertToBuffer("<div>Hello world!</div>");
         expect(buffer.slice(0, 4).toString()).toBe("%PDF");
     });
